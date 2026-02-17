d8197ff231e51480869b30050a68a34218fb25a1
AlpphA969 <pooyakarimi12@gmail.com>
2026-02-17 15:59:53 +0330

    Change login route from '/api/login' to '/login'

--- Tsak Managment System/BackEnd/routes/LoginRoutes.js
+++ Tsak Managment System/BackEnd/routes/LoginRoutes.js
@@ -2,5 +2,6 @@ const express = require('express')
 const { validate } = require('../middlewares/validate')
 const {LoginController}= require('../controllers/LoginController')
 const routes = express.Router()
-routes.post('/api/login' ,validate, LoginController )
+routes.post('/login' ,validate, LoginController )
+
 module.exports = routes
